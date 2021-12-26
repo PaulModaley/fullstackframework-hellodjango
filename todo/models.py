2cb0993bb2760af9efa894f7dc0542d7076c97ca
PaulModaley <pmodaley@gmail.com>
2021-12-26 14:38:27 +0000

    add loops to render data to HTML

--- todo/models.py
+++ todo/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Item(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
     done = models.BooleanField(null=False, blank=False)
@@ -8,4 +10,4 @@ class Item(models.Model):
     def __str__(self):
         return self.name
 
-
+        
